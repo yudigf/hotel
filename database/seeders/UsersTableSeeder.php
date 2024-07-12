cc04060c92e181c325ebab1f07dc9dc661a3f568
yudigf08 <yudi.fitory@wgs-id.com>
2024-07-12 09:07:15 +0700

    [Login]

---
+++ database/seeders/UsersTableSeeder.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+use DB;
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        DB::table('users')->insert([
+            //Admin
+            [
+                'name' => 'Admin',
+                'email' => 'admin@gmail.com',
+                'password' => HASH::make(111),
+                'role' => 'admin',
+                'status' => 'active',
+            ],
+
+            //User
+            [
+                'name' => 'User',
+                'email' => 'user@gmail.com',
+                'password' => HASH::make(111),
+                'role' => 'user',
+                'status' => 'active',
+            ],
+        ]);
+    }
+}
